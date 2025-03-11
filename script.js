4ee400a2353ae80bfde6556a3619ee13b774c341
Nick24234 <114647778+Nick24234@users.noreply.github.com>
2025-03-11 15:02:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-console.log("Лабораторна робота №2: Об’єктно-орієнтоване програмування у JavaScript");
 
-// Завдання 1.2.2
+console.log("Лабораторна робота №3: Застосування JavaScript для обробки даних");
+
+
 (function() {
     var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+    
     names.forEach(function(name) {
         var firstChar = name.charAt(0).toLowerCase();
         if (firstChar === 'j') {
             SpeakGoodBye(name);
         } else {
             SpeakHello(name);
+        }
+    });
+})();
+
+
+(function() {
+    var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+    
+    console.log("\n Селекціонування імен за кількістю літер. Імена з 4 або менше літерами отримують 'Hello', інші – 'Goodbye'.");
+    names.forEach(function(name) {
+        if (name.length <= 4) {
+            SpeakHello(name);
+        } else {
+            SpeakGoodBye(name);
         }
     });
 })();
